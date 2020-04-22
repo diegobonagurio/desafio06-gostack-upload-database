ca587eb2da1837e99ba9a34d286c0c781479170e
diegobonagurio <diego.bonagurio@gmail.com>
2020-04-21 22:31:41 -0300

    This is a really challenge

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,21 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepostory = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepostory.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction doesnot exist');
+    }
+
+    await transactionsRepostory.remove(transaction);
   }
 }
 
